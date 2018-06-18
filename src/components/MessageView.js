2b9ba0ede62fc4d6f1375f14cfa1859144abb3a0
Thierno Sadou Diallo <thierved01@gmail.com>
2018-06-18 17:50:52 +0000

    started to implement the remove message reducer

--- src/components/MessageView.js
+++ src/components/MessageView.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
 
+import {deleteMessage} from '../actions'
+
 class MessageView extends Component {
   render() {
+    
     return (
       <div id='messages'>
       {this.props.message.map((item, index) => {
-        return <p key={index}>{item}</p>
+        return <p key={index} 
+                  onClick={() => this.props.deleteMessage(this.props.message[0])}>{item}</p>
       })}
       </div>
     )
@@ -20,4 +24,8 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, null)(MessageView);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({deleteMessage}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageView);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,8 @@ const message = (state=[], action) => {
     switch (action.type) {
         case ADD_MESSAGE:
             return [...state, action.message];    
-        case DELETE_MESSAGE:
-            return state.slice(action.messageIndex, state.length);             
+        case DELETE_MESSAGE:            
+            return state.filter(item => item !== state[action.messageIndex]);             
         default:
             return state;
     }
